414dd4ace2844c0dcc8fb77335f2ea593e1a7b41
tamimalhridoy <tamimalhridoy3@gmail.com>
2025-02-08 23:46:00 +0600

    desing padding

--- router/index.js
+++ router/index.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const apiRouter = require("./api");
 const RenderUrl = require("../controllers/shorturl/renderUrl");
+const { homePage, registrationPage, LoginPage } = require("./staicSides");
 const router = express.Router();
 
 router.use("/api/v1", apiRouter);
 
-router.get("/", (req, res) => {
-  res.render("index");
-});
-
 router.get("/:shortID", RenderUrl);
+
+router.use("/", homePage);
+router.use("/registration", registrationPage);
+router.use("/login", LoginPage);
 
 router.use((req, res) => {
   res.send("pages not found.");
---
+++ router/staicSides/index.js
@@ -0,0 +1,13 @@
+const homePage = (req, res) => {
+  res.render("index");
+};
+
+const LoginPage = (req, res) => {
+  res.render("index");
+};
+
+const registrationPage = (req, res) => {
+  res.render("index");
+};
+
+module.exports = { homePage, LoginPage, registrationPage };
